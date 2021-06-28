25e46f55b6b2ddb9c5606f295541c69ff6aa676a
CreazyJack <913466287@qq.com>
2021-06-28 16:56:40 +0800

    1

--- src/components/Input/InputShow.tsx
+++ src/components/Input/InputShow.tsx
@@ -1,32 +1,33 @@
-import Input from '.'
-import Card from '../Cord'
+import React from 'react';
+import Input from '.';
+import Card from '../Cord';
 
 export default () => {
   return (
     <>
-      <Card title='不同大小'>
+      <Card title="不同大小">
         <div style={{ marginBottom: 16 }}>
-          <Input size='small' placeholder='small' />
+          <Input size="small" placeholder="small" />
         </div>
         <div style={{ marginBottom: 16 }}>
-          <Input placeholder='middle' />
+          <Input placeholder="middle" />
         </div>
         <div style={{ marginBottom: 16 }}>
-          <Input size='large' placeholder='large' />
+          <Input size="large" placeholder="large" />
         </div>
       </Card>
-      <Card title='前缀'>
-        <Input prefix='prefix' />
+      <Card title="前缀">
+        <Input prefix="prefix" />
       </Card>
-      <Card title='后缀'>
-        <Input suffix='suffix' />
+      <Card title="后缀">
+        <Input suffix="suffix" />
       </Card>
-      <Card title='前后缀'>
-        <Input prefix='prefix' suffix='suffix' />
+      <Card title="前后缀">
+        <Input prefix="prefix" suffix="suffix" />
       </Card>
-      <Card title='禁用'>
-        <Input disabled prefix='prefix' suffix='suffix' />
+      <Card title="禁用">
+        <Input disabled prefix="prefix" suffix="suffix" />
       </Card>
     </>
-  )
-}
+  );
+};
